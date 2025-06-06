6081bf2c8e1a66a960d9f1dce3e1b047bca44027
EnzoTr <enzotur05@hotmail.com>
2025-06-06 11:25:33 -0300

    port fix for render

--- server/index.js
+++ server/index.js
@@ -9,13 +9,20 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();    //  Permite acceder a las variables definidas en un archivo .env
 const app = express();  // Se crea una instancia del servidor express
 app.use(cors());    // Habilita las solicitudes de origen cruzado
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 app.use(express.json());    // Permite a Express procesar JSON.
 app.use("/api/products", productRoutes);
 
 
 
-app.listen(PORT,()=>{
-    connectDB();
-    console.log('Server started at http://localhost:' + PORT);
-})
+// start server
+const startServer = async () => {
+    try{
+        connectDB();
+        app.listen(8080, () => console.log('Server is running on port 8080'));
+    }catch (e){
+        console.error('Error starting server: ', e);
+    }
+};
+
+startServer();
